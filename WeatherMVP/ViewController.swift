16aa3459902a9c1ebf8acf2a16614d39b8f963bd
Ivan Volokitin <ivolokitin@bk.ru>
2020-08-21 21:58:39 +0300

    Initial Commit

---
+++ WeatherMVP/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WeatherMVP
+//
+//  Created by Ivan Volokitin on 21.08.2020.
+//  Copyright © 2020 Ivan Volokitin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
